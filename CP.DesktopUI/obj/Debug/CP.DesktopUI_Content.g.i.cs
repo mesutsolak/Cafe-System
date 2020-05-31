333bdd0c3439191bcbab6a23cdd87a20168a1e04
Mesut Solak <mesuttsolak@gmail.com>
2020-05-31 21:38:20 +0300

    Wpf iconun değiştirilmesi

--- CP.DesktopUI/obj/Debug/CP.DesktopUI_Content.g.i.cs
+++ CP.DesktopUI/obj/Debug/CP.DesktopUI_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/cafe.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("cafe.ico")]
 
 
