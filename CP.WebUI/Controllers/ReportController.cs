a742a36646785ac1c1704917e8b209bc2769e9fa
Mesut Solak <mesuttsolak@gmail.com>
2020-06-03 11:07:06 +0300

    .rdlc nesnelerinin oluşturulması

--- CP.WebUI/Controllers/ReportController.cs
+++ CP.WebUI/Controllers/ReportController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CP.WebUI.Reports.Dataset;
+using Microsoft.Reporting.WebForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,13 +8,35 @@ using System.Web.Mvc;
 
 namespace CP.WebUI.Controllers
 {
-    public class ReportController : Controller
+    public class ReportController : BaseController
     {
+        ReportDataset _rd = new ReportDataset();
         // GET: Report
         [Route("Raporlar")]
         public ActionResult Index()
         {
             return View();
+        }
+        [Route("RaportList")]
+        public PartialViewResult ReportView(string name)
+        {
+
+
+            return PartialView();
+        }
+        public void ReportViewOperation()
+        {
+            ReportViewer reportViewer = new ReportViewer
+            {
+                ProcessingMode = ProcessingMode.Local,
+                AsyncRendering = true,
+                SizeToReportContent = true,
+                //Width = Unit.Percentage(1500),
+                //Height = Unit.Percentage(1500),
+                ShowExportControls = true
+            };
+        
+
         }
     }
 }
